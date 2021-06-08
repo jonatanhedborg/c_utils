087b0b24fff7abc7e0e52e769deffca5f7069040
Mattias <mattias@mattiasgustavsson.com>
2021-06-08 12:45:03 +0200

    Added #ifdef for intmap thread safety

--- source/c_utils/intmap.h
+++ source/c_utils/intmap.h
@@ -1,6 +1,8 @@
 #ifndef intmap_h
 #define intmap_h
 
+// To make intmap thread safe, do this before include: #define INTMAP_THREAD_SAFE
+
 typedef struct intmap_t intmap_t;
 
 intmap_t* intmap_create( int item_size );
@@ -35,69 +37,89 @@ static uint32_t intmap_hash_u32( uint32_t key ) {
 typedef struct intmap_t {
     hashtable_t hashtable;
     int item_size;
-    thread_mutex_t mutex;
+    #ifdef INTMAP_THREAD_SAFE
+        thread_mutex_t mutex;
+    #endif
 } intmap_t;
+
+
+#ifdef INTMAP_THREAD_SAFE
+    #define INTMAP_MUTEX_LOCK(x) thread_mutex_lock( (x) )
+    #define INTMAP_MUTEX_UNLOCK(x) thread_mutex_unlock( (x) )
+#else
+    #define INTMAP_MUTEX_LOCK(x) 
+    #define INTMAP_MUTEX_UNLOCK(x) 
+#endif
 
 
 intmap_t* intmap_create( int item_size ) {
     intmap_t* intmap = (intmap_t*) malloc( sizeof( intmap_t ) );
     int const initial_capacity = 256;
     hashtable_init( &intmap->hashtable, sizeof( int ), item_size,  initial_capacity, NULL );
-    thread_mutex_init( &intmap->mutex );
+    #ifdef INTMAP_THREAD_SAFE
+        thread_mutex_init( &intmap->mutex );
+    #endif
     intmap->item_size = item_size;
     return intmap;
 }
 
 
 void intmap_destroy( intmap_t* intmap ) {
-    thread_mutex_lock( &intmap->mutex );
+    INTMAP_MUTEX_LOCK( &intmap->mutex );
     hashtable_term( &intmap->hashtable );
-    thread_mutex_unlock( &intmap->mutex );    
-    thread_mutex_term( &intmap->mutex );
+    INTMAP_MUTEX_UNLOCK( &intmap->mutex );    
+    #ifdef INTMAP_THREAD_SAFE
+        thread_mutex_term( &intmap->mutex );
+    #endif
     free( intmap );
 }
 
 
 void intmap_clear( intmap_t* intmap ) {
-    thread_mutex_lock( &intmap->mutex );
+    INTMAP_MUTEX_LOCK( &intmap->mutex );
     hashtable_clear( &intmap->hashtable );
-    thread_mutex_unlock( &intmap->mutex );    
+    INTMAP_MUTEX_UNLOCK( &intmap->mutex );    
 }
 
 
 void intmap_insert( intmap_t* intmap, int key, void const* item ) {
-    thread_mutex_lock( &intmap->mutex );
+    INTMAP_MUTEX_LOCK( &intmap->mutex );
     hashtable_insert( &intmap->hashtable, intmap_hash_u32( key ), &key, item );
-    thread_mutex_unlock( &intmap->mutex );    
+    INTMAP_MUTEX_UNLOCK( &intmap->mutex );    
 }
 
 
 void intmap_remove( intmap_t* intmap, int key ) {
-    thread_mutex_lock( &intmap->mutex );
+    INTMAP_MUTEX_LOCK( &intmap->mutex );
     hashtable_remove( &intmap->hashtable, intmap_hash_u32( key ), &key );
-    thread_mutex_unlock( &intmap->mutex );    
+    INTMAP_MUTEX_UNLOCK( &intmap->mutex );    
 }
 
 
 bool intmap_update( intmap_t* intmap, int key, void const* item ) {
-    thread_mutex_lock( &intmap->mutex );
+    INTMAP_MUTEX_LOCK( &intmap->mutex );
     void* result = hashtable_find( &intmap->hashtable, intmap_hash_u32( key ), &key );
     if( result ) {
         memcpy( result, item, (size_t) intmap->item_size );
     }
-    thread_mutex_unlock( &intmap->mutex );    
+    INTMAP_MUTEX_UNLOCK( &intmap->mutex );    
     return result != NULL;
 }
 
 
 bool intmap_find( intmap_t* intmap, int key, void* item ) {
-    thread_mutex_lock( &intmap->mutex );
+    INTMAP_MUTEX_LOCK( &intmap->mutex );
     void const* result = hashtable_find( &intmap->hashtable, intmap_hash_u32( key ), &key );
     if( result ) {
         memcpy( item, result, (size_t) intmap->item_size );
     }
-    thread_mutex_unlock( &intmap->mutex );    
+    INTMAP_MUTEX_UNLOCK( &intmap->mutex );    
     return result != NULL;
 }
+
+
+#undef INTMAP_MUTEX_LOCK
+#undef INTMAP_MUTEX_UNLOCK
+
 
 #endif /* INTMAP_IMPLEMENTATION */
